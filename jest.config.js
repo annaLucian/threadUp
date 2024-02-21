838f643ca7e9eef835db41f55d2a51c643c70fd0
annaLucian <garielapoves01@gmail.com>
2024-02-21 11:01:04 +0100

    jest, tailwind dependencies installed

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+export default {
+  testEnvironment: "jsdom",
+  setupFilesAfterEnv: ["<rootDir>/setupTests.js"],
+  // moduleNameMapper: {
+  //   "^.+\\.svg$": "jest-svg-transformer",
+  //   "^.+\\.(css|less|scss)$": "identity-obj-proxy",
+  // },
+};
