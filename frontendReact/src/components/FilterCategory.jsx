32eac6ae2a4e625ae888e6798901283e87b47b98
annaLucian <garielapoves01@gmail.com>
2024-02-21 20:13:50 +0100

    feat: :rocket: the product filter was implemented by gender

--- frontendReact/src/components/FilterCategory.jsx
+++ frontendReact/src/components/FilterCategory.jsx
@@ -3,23 +3,37 @@ import iconShirt from "../assets/icon-shirt.svg";
 import { Link } from "react-router-dom";
 export default function FilterCategory() {
   return (
-    <section>
-      <h1>
-        ¿Qué estás buscando? Explora nuestra amplia selección de moda para
-        hombres y mujeres
-      </h1>
-      <div>
-        <Link to="/">
+    <section className="flex flex-col justify-center items-center pt-[2.5rem]">
+      <h1 className="text-[1.8rem] font-bold">¿Qué estás buscando?</h1>
+      <span className="text-[1.2rem]">
+        Explora nuestra amplia selección de moda para hombres y mujeres
+      </span>
+      <div className="pt-[2rem] flex gap-[2rem] items-center justify-center">
+        <Link
+          to="/list-products?search=hombre"
+          className="text-center flex flex-col items-center p-[0.5rem] hover:bg-[#efefef]"
+        >
           <picture>
-            <img src={iconShirt} alt="icono de camisa" />
+            <img
+              src={iconShirt}
+              alt="icono de camisa"
+              className="w-[2.5rem] mb-[0.4rem]"
+            />
           </picture>
-          <p>Hombres</p>
+          <p className="text-[0.8rem]">Hombres</p>
         </Link>
-        <Link to="/">
+        <Link
+          to="/list-products?search=mujer"
+          className="text-center flex flex-col items-center p-[0.5rem] hover:bg-[#efefef]"
+        >
           <picture>
-            <img src={iconDress} alt="icono de vestido" />
+            <img
+              src={iconDress}
+              alt="icono de vestido"
+              className="w-[2.5rem] mb-[0.4rem]"
+            />
           </picture>
-          <p>Mujeres</p>
+          <p className="text-[0.8rem]">Mujeres</p>
         </Link>
       </div>
     </section>
--- frontendReact/src/main.jsx
+++ frontendReact/src/main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import ErrorPage from "./pages/error/Error.jsx";
 import CreateForm from "./pages/createForm/CreateForm.jsx";
+import ListProducts from "./pages/listProducts/ListProducts.jsx";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import App from "./App.jsx";
@@ -16,6 +17,10 @@ const router = createBrowserRouter([
   {
     path: "publish-product",
     element: <CreateForm />,
+  },
+  {
+    path: "list-products",
+    element: <ListProducts />,
   },
 ]);
 
