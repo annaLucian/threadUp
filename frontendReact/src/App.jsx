c4e3a71b8562ebc2f3590685d95c8928abfcd89d
annaLucian <garielapoves01@gmail.com>
2024-02-21 16:54:31 +0100

    feat: :lipstick: installing fonts and stilyng basic

--- frontendReact/src/App.jsx
+++ frontendReact/src/App.jsx
@@ -1,8 +1,9 @@
-import Cardproduct from ""
-
+import FilterCategory from "./components/FilterCategory.jsx";
 
 export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
-
-   <CardProduct />
+  return (
+    <>
+      <FilterCategory />
+    </>
+  );
 }
