aa52ee4b8f6a82b3a1e6f808f0a69b9b27724b27
annaLucian <garielapoves01@gmail.com>
2024-02-21 17:43:30 +0100

    feat: :rocket: create new component filterCategory

--- frontendReact/src/components/FilterCategory.jsx
+++ frontendReact/src/components/FilterCategory.jsx
@@ -1,3 +1,27 @@
+import iconDress from "../assets/icon-dress.svg";
+import iconShirt from "../assets/icon-shirt.svg";
+import { Link } from "react-router-dom";
 export default function FilterCategory() {
-  return <p>Soy un filter</p>;
+  return (
+    <section>
+      <h1>
+        ¿Qué estás buscando? Explora nuestra amplia selección de moda para
+        hombres y mujeres
+      </h1>
+      <div>
+        <Link to="/">
+          <picture>
+            <img src={iconShirt} alt="icono de camisa" />
+          </picture>
+          <p>Hombres</p>
+        </Link>
+        <Link to="/">
+          <picture>
+            <img src={iconDress} alt="icono de vestido" />
+          </picture>
+          <p>Mujeres</p>
+        </Link>
+      </div>
+    </section>
+  );
 }
