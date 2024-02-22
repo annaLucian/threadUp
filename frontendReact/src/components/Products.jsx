a6fc5370002819dc27f47c0bc1849c599daa12ec
annaLucian <garielapoves01@gmail.com>
2024-02-22 14:38:09 +0100

    feat: add pagine details

--- frontendReact/src/components/Products.jsx
+++ frontendReact/src/components/Products.jsx
@@ -2,12 +2,12 @@ import { Card, CardBody, Typography } from "@material-tailwind/react";
 import { useEffect, useState } from "react";
 import Services from "./services.js";
 import HeartToggle from "./HeartToggle.jsx";
+import { Link, useAsyncError } from "react-router-dom";
 
 const cardProduct = Services;
 
 function Product() {
   const [products, setProducts] = useState([]);
-
   useEffect(() => {
     const fetchProducts = async () => {
       const productsData = await cardProduct.getProducts();
@@ -26,38 +26,40 @@ function Product() {
       </p>
       <div className="grid grid-cols-2 lg:grid-cols-4 gap-4">
         {products.map((product, id) => (
-          <Card
-            key={id}
-            className="bg-customWhite container-card-product mt-4 max-w-xs rounded-[10px] shadow-x1 "
-          >
-            <CardBody className="p-4">
-              <>
-                <HeartToggle />
-              </>
+          <Link to={`/list-products?search=${id}`}>
+            <Card
+              key={id}
+              className="bg-customWhite container-card-product mt-4 max-w-xs rounded-[10px] shadow-x1 "
+            >
+              <CardBody className="p-4">
+                <>
+                  <HeartToggle />
+                </>
 
-              <img
-                src={`https://threadup-iajq.onrender.com/${product.image}`}
-                alt={product.title}
-                className="w-48 h-48 mx-auto mb-4"
-              />
-              <Typography className="text-xl sm:text-lg mb-2 text-center font-bold">
-                {product.productName}
-              </Typography>
+                <img
+                  src={`https://threadup-iajq.onrender.com/${product.image}`}
+                  alt={product.title}
+                  className="w-48 h-48 mx-auto mb-4"
+                />
+                <Typography className="text-xl sm:text-lg mb-2 text-center font-bold">
+                  {product.productName}
+                </Typography>
 
-              <div className="grid grid-cols-2 text-center">
-                <Typography className="text-[16px]sm:text-lg mb-2 font-bold">
-                  {" "}
-                  Talla:
-                  {product.size}
-                </Typography>
-                <Typography className="text-[16px]text-sm font-bold">
-                  {" "}
-                  Precio:
-                  {product.price}€
-                </Typography>
-              </div>
-            </CardBody>
-          </Card>
+                <div className="grid grid-cols-2 text-center">
+                  <Typography className="text-[16px]sm:text-lg mb-2 font-bold">
+                    {" "}
+                    Talla:
+                    {product.size}
+                  </Typography>
+                  <Typography className="text-[16px]text-sm font-bold">
+                    {" "}
+                    Precio:
+                    {product.price}€
+                  </Typography>
+                </div>
+              </CardBody>
+            </Card>
+          </Link>
         ))}
       </div>
     </div>
