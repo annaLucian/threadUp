625d022bbf6422702c715eec3fe64b590f88f579
annaLucian <garielapoves01@gmail.com>
2024-02-22 10:05:37 +0100

    feat: :bug: solution conflicts

--- frontendReact/src/App.jsx
+++ frontendReact/src/App.jsx
@@ -1,24 +1,10 @@
 import FilterCategory from "./components/FilterCategory.jsx";
-
+import Products from "./components/Products.jsx";
 export default function App() {
   return (
     <>
       <FilterCategory />
-    </>
-  );
-import Products from "./components/Products.jsx"
-
-
-export default function App() {
-  return (
-  
-  <>
-    <Products />
-  </>
-  )
-  return (
-    <>
-      <FilterCategory />
+      <Products />
     </>
   );
 }
